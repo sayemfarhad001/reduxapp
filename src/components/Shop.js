6df611c3a090b95b2539d99ff3a2949100cd5900
Sayem Farhad <sayemfarhad001@gmail.com>
2024-10-01 19:18:25 -0400

    make folder structure for AZ Bank

---
+++ src/components/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+        <h2>Buy Puma Shoes $100</h2>
+      <buttn className="btn btn-primary">-</buttn>
+        Add to cart
+      <buttn className="btn btn-primary">+</buttn>
+    </div>
+  )
+}
+
+export default Shop
